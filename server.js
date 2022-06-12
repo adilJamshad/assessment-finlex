ad1739f8a4d227391e4c801baf0c33b821a184a5
adilJamshad <jamshad.adil@gmail.com>
2022-06-13 00:40:15 +0500

    added cors

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 //Install express server
 const express = require("express");
 const path = require("path");
-
+const cors = require("cors");
 const app = express();
-
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + "/dist/assessment"));
+app.use(cors());
 
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/assessment/index.html"));
